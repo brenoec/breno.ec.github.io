2c1ba534b10c981e63c0d5645169a5791fbbc0df
Breno <breno.ec@gmail.com>
2014-11-16 17:20:31 -0200

    directions api

--- js/maps.js
+++ js/maps.js
@@ -3,27 +3,50 @@ var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
 
-function initialize() {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      directionsDisplay = new google.maps.DirectionsRenderer();
-      var location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-      var mapOptions = {
-        zoom: 17,
-        center: location
-      };
-      map = new google.maps.Map(document.getElementById('maps'), mapOptions);
-      directionsDisplay.setMap(map);
-    });
-  } else {
+function renderMap() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var location = new google.maps.LatLng(-19.920682, -43.920128);
+  var mapOptions = {
+    zoom: 17,
+    center: location
+  };
+  map = new google.maps.Map(document.getElementById('maps'), mapOptions);
+  directionsDisplay.setMap(map);
+}
+
+function calcRoute(latitute, longitude) {
+  var request = {
+      origin: (latitude + ',' + longitude),
+      destination: '-19.920682,-43.920128',
+      travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
+
+function renderGeolocationMap() {
+  navigator.geolocation.getCurrentPosition(function(position) {
     directionsDisplay = new google.maps.DirectionsRenderer();
-    var location = new google.maps.LatLng(-19.920682, -43.920128);
+    var location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
     var mapOptions = {
       zoom: 17,
       center: location
     };
     map = new google.maps.Map(document.getElementById('maps'), mapOptions);
     directionsDisplay.setMap(map);
+
+    calcRoute(position.coords.latitude, position.coords.longitude);
+  });
+}
+
+function initialize() {
+  if ("geolocation" in navigator) {
+    renderGeolocationMap();
+  } else {
+    renderMap();
   }
 }
 google.maps.event.addDomListener(window, 'load', initialize);
