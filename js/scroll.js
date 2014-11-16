dfe894c2430b2a7c755c0261c6beb81e7683c285
Breno <breno.ec@gmail.com>
2014-11-16 18:21:03 -0200

    hotfix

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,5 @@
 
-if (navigator.userAgent.indexOf('Mobi') === -1) {
+if (navigator.userAgent.indexOf('Mobi') < 0) {
   $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
