c092a9049e5c3af0e476a471483cb55dede5eb95
Breno <breno.ec@gmail.com>
2014-11-17 00:04:43 -0200

    weather

--- js/maps.js
+++ js/maps.js
@@ -15,11 +15,23 @@ function renderMap() {
 }
 
 function calcRoute(latitude, longitude) {
-  var request = {
-      origin: (latitude + ',' + longitude),
-      destination: '-19.920682,-43.920128',
-      travelMode: google.maps.TravelMode.DRIVING
-  };
+  var today = new Date();
+  var request;
+
+  if (today.getHours() >= 18){
+    request = {
+        origin: (latitude + ',' + longitude),
+        destination: '-19.920682,-43.920128',
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+  } else {
+    request = {
+        origin: (latitude + ',' + longitude),
+        destination: '-19.857774,-43.959072',
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+  }
+
   directionsService.route(request, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(response);
